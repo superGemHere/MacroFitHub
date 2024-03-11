fc1f4293182e10bf5b074f07eb6edba467b51518
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-03-11 14:19:17 +0200

    first steps project creating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { Routes } from 'react-router-dom'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    
+    <div className="app">
+
+      <Routes>
+        
+      </Routes>
+
+    </div>
+
   )
 }
 
