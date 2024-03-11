b5b6010c43232eb59aacdf2685f1766eb821bf1d
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-03-11 21:32:44 +0200

    add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 
 import './App.css'
+import Header from './components/outlet/Navbar/Header';
+import Home from './components/main/Home/Home';
 
 function App() {
 
   return (
     
     <div className="app">
-
+      <Header />
       <Routes>
-        
+        <Route path='/' element={<Home />} />
       </Routes>
 
     </div>
