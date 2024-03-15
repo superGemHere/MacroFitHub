1af3647e4c9ad407099e6a91010484c88e5572da
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-03-15 16:29:16 +0200

    test layout

---
+++ client/src/components/main/Catalog/Catalog.jsx
@@ -0,0 +1,88 @@
+import "./catalog.scss";
+
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+
+export default  function Catalog() {
+    
+    
+    return (
+        <div className="catalog">
+            <div className="container">
+                <div className="card" style={{backgroundImage: `url(https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                    <h2>Name</h2>
+                    <div className="buttons">
+                    <InfoOutlinedIcon className="info-btn"/>
+                    <AddOutlinedIcon className="add-btn"/>
+                    </div>
+                </div>
+               
+                <div className="card" style={{backgroundImage: `url(https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                    <h2>Name</h2>
+                    <div className="buttons">
+                    <InfoOutlinedIcon className="info-btn"/>
+                    <AddOutlinedIcon className="add-btn"/>
+                    </div>
+                </div>
+               
+                <div className="card" style={{backgroundImage: `url(https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                    <h2>Name</h2>
+                    <div className="buttons">
+                    <InfoOutlinedIcon className="info-btn"/>
+                    <AddOutlinedIcon className="add-btn"/>
+                    </div>
+                </div>
+               
+                <div className="card" style={{backgroundImage: `url(https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                    <h2>Name</h2>
+                    <div className="buttons">
+                    <InfoOutlinedIcon className="info-btn"/>
+                    <AddOutlinedIcon className="add-btn"/>
+                    </div>
+                </div>
+               
+                <div className="card" style={{backgroundImage: `url(https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                    <h2>Name</h2>
+                    <div className="buttons">
+                    <InfoOutlinedIcon className="info-btn"/>
+                    <AddOutlinedIcon className="add-btn"/>
+                    </div>
+                </div>
+               
+                <div className="card" style={{backgroundImage: `url(https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                    <h2>Name</h2>
+                    <div className="buttons">
+                    <InfoOutlinedIcon className="info-btn"/>
+                    <AddOutlinedIcon className="add-btn"/>
+                    </div>
+                </div>
+               
+                <div className="card" style={{backgroundImage: `url(https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                    <h2>Name</h2>
+                    <div className="buttons">
+                    <InfoOutlinedIcon className="info-btn"/>
+                    <AddOutlinedIcon className="add-btn"/>
+                    </div>
+                </div>
+               
+                <div className="card" style={{backgroundImage: `url(https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                    <h2>Name</h2>
+                    <div className="buttons">
+                    <InfoOutlinedIcon className="info-btn"/>
+                    <AddOutlinedIcon className="add-btn"/>
+                    </div>
+                </div>
+               
+                <div className="card" style={{backgroundImage: `url(https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                    <h2>Name</h2>
+                    <div className="buttons">
+                    <InfoOutlinedIcon className="info-btn"/>
+                    <AddOutlinedIcon className="add-btn"/>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+    )
+
+}
