965120ddac9cf9bc92bf7e1f2ee35e86622526ab
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-03-17 14:31:56 +0200

    Update Catalog.jsx

--- client/src/components/main/Catalog/Catalog.jsx
+++ client/src/components/main/Catalog/Catalog.jsx
@@ -28,6 +28,30 @@ export default  function Catalog() {
             name: 'Chicken Breast Grill',
             img: "https://images.pexels.com/photos/262945/pexels-photo-262945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 
+        },
+        {
+            id: '5',
+            name: 'Beef Steak',
+            img: "https://images.pexels.com/photos/65175/pexels-photo-65175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            
+        },
+        {
+            id: '6',
+            name: 'Oathmeal',
+            img: "https://images.pexels.com/photos/4725726/pexels-photo-4725726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+        },
+        {
+            id: '7',
+            name: 'Boiled Eggs',
+            img: "https://images.pexels.com/photos/806457/pexels-photo-806457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+        },
+        {
+            id: '8',
+            name: 'Chicken Breast Grill',
+            img: "https://images.pexels.com/photos/262945/pexels-photo-262945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
         }
     ]
     
