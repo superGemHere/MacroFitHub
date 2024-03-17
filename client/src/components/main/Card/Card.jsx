908b75cc3235459593027d0b0601a42e54a29a48
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-03-17 02:17:01 +0200

    make interactive card switch between add-form and btns

--- client/src/components/main/Card/Card.jsx
+++ client/src/components/main/Card/Card.jsx
@@ -2,18 +2,33 @@ import "./card.scss"
 
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import AddProduct from "../AddProduct/AddProduct";
+import { useState } from "react";
+
+
 
 export default function Card ({
     item,
 }){
+
+    const [isVis, setIsVis] = useState(false);
+
+    const showAdd = () => {
+        
+    }
                             
     return(
         <div className="card"  style={{backgroundImage: `url(${item.img})`}}>
-            <h2>{item.name}</h2>
-            <div className="buttons">
-                <InfoOutlinedIcon className="info-btn"/>
-                <AddOutlinedIcon className="add-btn"/>
-            </div>
+            <h2 style={isVis ? {top: "0px"} : {}}>{item.name}</h2>
+            {isVis  ? 
+                    <AddProduct  setIsVis={setIsVis} item={item}/>
+                    :   
+                    <div className="buttons">
+                        <InfoOutlinedIcon className="info-btn"/>
+                        <AddOutlinedIcon className="add-btn" onClick={() => setIsVis(true)}/>
+                    </div>
+                    
+                    }
         </div>				
     );
 }
