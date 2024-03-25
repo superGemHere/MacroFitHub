b23d1247d7a7414497e658cb69fd7fe8c30cea60
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-03-25 21:18:30 +0200

    add info btn action +  append InfoProduct

--- client/src/components/main/Card/Card.jsx
+++ client/src/components/main/Card/Card.jsx
@@ -3,6 +3,7 @@ import "./card.scss"
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import AddProduct from "../AddProduct/AddProduct";
+import InfoProduct from "../InfoProduct/InfoProduct";
 import { useState } from "react";
 
 
@@ -11,22 +12,21 @@ export default function Card ({
     item,
 }){
 
-    const [isVis, setIsVis] = useState(false);
+    const [isAddVis, setIsAddVis] = useState(false);
+    const [isInfoVis, setIsInfoVis] = useState(false);
 
-    const showAdd = () => {
-        
-    }
                             
     return(
-        <div className="card"  style={  isVis ? {backgroundImage: `url(${item.img})`, padding: "0"}: {backgroundImage: `url(${item.img})`, } }>
-            {isVis  ? 
-                    <AddProduct  setIsVis={setIsVis} item={item}/>
-                    :   
-                    <>
-                        <h2 style={isVis ? {top: "0px"} : {}}>{item.name}</h2>
+        <div className="card"  style={  isAddVis ? {backgroundImage: `url(${item.img})`, padding: "0"}: isInfoVis ? {backgroundImage: `url(${item.img})`, padding: "0", height: "calc(25em + 4em)"} : {backgroundImage: `url(${item.img})`, } }>
+            {isAddVis  ? 
+                    <AddProduct  setIsAddVis={setIsAddVis} item={item}/>
+                    : isInfoVis 
+                    ? <InfoProduct setIsInfoVis={setIsInfoVis} item={item}/>    
+                    :<>
+                        <h2 style={isAddVis ? {top: "0px"} : {}}>{item.name}</h2>
                     <div className="buttons">
-                        <InfoOutlinedIcon className="info-btn"/>
-                        <AddOutlinedIcon className="add-btn" onClick={() => setIsVis(true)}/>
+                        <InfoOutlinedIcon className="info-btn" onClick = {() => setIsInfoVis(true)}/>
+                        <AddOutlinedIcon className="add-btn" onClick={() => setIsAddVis(true)}/>
                     </div>
                     </>
                     
